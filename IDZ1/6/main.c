e91510d74c3c6a6c4269b83c51c3531359edf79a
Ilya <kramnik.i2003@gmail.com>
2023-03-19 18:48:19 +0300

    added programs on mark 6 and 7

--- IDZ1/6/main.c
+++ IDZ1/6/main.c
@@ -17,10 +17,11 @@ int main(int argc, char *argv[]) {
     char *input_file = (char *) calloc(5000, sizeof(char)); // название файла для чтения
     char *output_file = (char *) calloc(5000, sizeof(char)); // название файла для записи
     char temp;
+    int *status;
     ssize_t read_bytes;
     size_t write_size, pipe_size;
 
-    pid_t pid, chpid, chpid2;
+    pid_t chpid, chpid2;
 
     strcpy(input_file, argv[3]);
     strcpy(output_file, argv[4]);
@@ -32,7 +33,13 @@ int main(int argc, char *argv[]) {
         printf("Can\'t create pipe\n");
         exit(-1);
     }
-    pid = getpid();
+    if (pipe(fd_pipe2) < 0) { // создание второго pipe
+        /* Если создать pipe2 не удалось, печатаем об этом сообщение
+        и прекращаем работу */
+        printf("Can\'t create pipe2\n");
+        exit(-1);
+    }
+
     chpid = fork(); // создание второго процесса
 
     if (chpid < 0) {
@@ -68,7 +75,8 @@ int main(int argc, char *argv[]) {
         /* Закрываем входной поток данных, на этом
         родитель прекращает работу */
         close(fd_pipe[1]);
-    } else if (chpid == 0) { // второй процесс
+    }
+    else if (chpid == 0) { // второй процесс
         // Пробуем прочитать из pipe данные в массив
         close(fd_pipe[1]);
         pipe_size = read(fd_pipe[0], buf, 5000);
@@ -85,60 +93,51 @@ int main(int argc, char *argv[]) {
             buf[i] = buf[n2 - (i - n1)];
             buf[n2 - (i - n1)] = temp;
         }
+
+        // записываем измененную строку во второй pipe
+        close(fd_pipe2[0]);
+        pipe_size = write(fd_pipe2[1], buf, strlen(buf));
+        if (pipe_size != strlen(buf)) {
+            /* Если записалось меньшее количество байт, сообщаем
+            об ошибке и завершаем работу */
+            printf("child Can\'t write all string\n");
+            exit(-1);
+        }
+        /* Закрываем входной поток данных, на этом
+        родитель прекращает работу */
+        close(fd_pipe2[1]);
+    }
+
+    if (chpid > 0) { // возвращаемся в первый процесс
+        wait(status);
+        // Пробуем прочитать из второго pipe данные в массив
+        close(fd_pipe2[1]);
+        pipe_size = read(fd_pipe2[0], buf, strlen(buf));
+        if (pipe_size < 0) {
+            /* Если прочитать не смогли, сообщаем об ошибке и
+            завершаем работу */
+            printf("Can\'t read string\n");
+            exit(-1);
+        }
+        close(fd_pipe2[0]);
         printf("%s\n", buf);
 
-        // создание второго pipe
-        if (pipe(fd_pipe2) < 0) {
-            /* Если создать pipe2 не удалось, печатаем об этом сообщение
-            и прекращаем работу */
-            printf("Can\'t create pipe2\n");
+        if ((fd2 = open(output_file, O_CREAT | O_WRONLY, 0666)) <
+            0) { //имя файла, что делаем с файлом, какие права предоставляем владельцу, группе и пользователю
+            printf("Can\'t open file for writing\n");
             exit(-1);
         }
 
-        chpid2 = fork(); // создание третьего процесса
+        // записываем измененную строку в новый файл
+        write_size = write(fd2, buf, strlen(buf));
 
-        if (chpid2 > 0) {
-            // записываем измененную строку во второй pipe
-            close(fd_pipe2[0]);
-            pipe_size = write(fd_pipe2[1], buf, strlen(buf));
-            if (pipe_size != strlen(buf)) {
-                /* Если записалось меньшее количество байт, сообщаем
-                об ошибке и завершаем работу */
-                printf("child Can\'t write all string\n");
-                exit(-1);
-            }
-            /* Закрываем входной поток данных, на этом
-            родитель прекращает работу */
-            close(fd_pipe2[1]);
-        } else if (chpid2 == 0) { // третий процесс
-            if ((fd2 = open(output_file, O_CREAT | O_WRONLY, 0666)) <
-                0) { //имя файла, что делаем с файлом, какие права предоставляем владельцу, группе и пользователю
-                printf("Can\'t open file for writing\n");
-                exit(-1);
-            }
+        if (write_size != strlen(buf)) {
+            printf("2nd child Can\'t write all string\n");
+            exit(-1);
+        }
 
-            // Пробуем прочитать из второго pipe данные в массив
-            close(fd_pipe2[1]);
-            pipe_size = read(fd_pipe2[0], buf, buf_size);
-            if (pipe_size < 0) {
-                /* Если прочитать не смогли, сообщаем об ошибке и
-                завершаем работу */
-                printf("Can\'t read string\n");
-                exit(-1);
-            }
-            close(fd_pipe2[0]);
-
-            // записываем измененную строку в новый файл
-            write_size = write(fd2, buf, strlen(buf));
-
-            if (write_size != strlen(buf)) {
-                printf("2nd child Can\'t write all string\n");
-                exit(-1);
-            }
-
-            if (close(fd2) < 0) {
-                printf("Can\'t close file\n");
-            }
+        if (close(fd2) < 0) {
+            printf("Can\'t close file\n");
         }
     }
 }
--- IDZ1/7/main.c
+++ IDZ1/7/main.c
@@ -38,6 +38,15 @@ int main(int argc, char *argv[]) {
         printf("Can\'t create fifo at process 1\n");
         exit(-1);
     }
+    // создание fifo2
+    (void) umask(0);
+    if (mknod(fifo_name2, S_IFIFO | 0666, 0) < 0) {
+        /* Если создать fifo2 не удалось, печатаем об этом
+        сообщение и прекращаем работу */
+        printf("Can\'t create fifo at process 2\n");
+        exit(-1);
+    }
+
     chpid = fork(); // создание второго процесса
 
     if (chpid < 0) {
@@ -103,72 +112,60 @@ int main(int argc, char *argv[]) {
         }
         printf("%s\n", buf);
 
-        // создание fifo2
-        (void) umask(0);
-        if (mknod(fifo_name2, S_IFIFO | 0666, 0) < 0) {
-            /* Если создать fifo2 не удалось, печатаем об этом
+        if ((fd_fifo2 = open(fifo_name2, O_WRONLY)) < 0) {
+            /* Если открыть fifo2 не удалось, печатаем об этом
             сообщение и прекращаем работу */
-            printf("Can\'t create fifo at process 2\n");
+            printf("Can\'t open FIFO for writing at second process\n");
+            exit(-1);
+        }
+        // Пробуем записать в fifo2 наш buf
+        fifo_size = write(fd_fifo2, buf, strlen(buf));
+        if (fifo_size != strlen(buf)) {
+            /* Если записалось меньшее количество байт, то сообщаем
+            об ошибке и завершаем работу */
+            printf("Can\'t write all string to fifo2 at second process\n");
+            exit(-1);
+        }
+        /* Закрываем входной поток данных и на этом родитель
+        прекращает работу */
+        close(fd_fifo2);
+    }
+    if (chpid > 0) { // снова первый процесс
+        if ((fd2 = open(output_file, O_CREAT | O_WRONLY, 0666)) <
+            0) { //имя файла, что делаем с файлом, какие права предоставляем владельцу, группе и пользователю
+            printf("Can\'t open file for writing\n");
             exit(-1);
         }
 
-        chpid2 = fork(); // создание третьего потока
-        if (chpid2 > 0) {
-            if ((fd_fifo2 = open(fifo_name2, O_WRONLY)) < 0) {
-                /* Если открыть fifo не удалось, печатаем об этом
-                сообщение и прекращаем работу */
-                printf("Can\'t open FIFO for writing at second process\n");
-                exit(-1);
-            }
-            // Пробуем записать в fifo2 наш buf
-            fifo_size = write(fd_fifo2, buf, strlen(buf));
-            if (fifo_size != strlen(buf)) {
-                /* Если записалось меньшее количество байт, то сообщаем
-                об ошибке и завершаем работу */
-                printf("Can\'t write all string to fifo2 at second process\n");
-                exit(-1);
-            }
-            /* Закрываем входной поток данных и на этом родитель
-            прекращает работу */
-            close(fd_fifo2);
-        } else if (chpid2 == 0) { // третий процесс
-
-            if ((fd2 = open(output_file, O_CREAT | O_WRONLY, 0666)) <
-                0) { //имя файла, что делаем с файлом, какие права предоставляем владельцу, группе и пользователю
-                printf("Can\'t open file for writing\n");
-                exit(-1);
-            }
-
-            // Пробуем прочитать из fifo2 данные в массив
-            if ((fd_fifo2 = open(fifo_name2, O_RDONLY)) < 0) {
-                /* Если открыть fifo2 не удалось, печатаем об этом
-                сообщение и прекращаем работу */
-                printf("Can\'t open fifo2 for reading at third process\n");
-                exit(-1);
-            }
-
-            fifo_size = read(fd_fifo2, buf, 5000);
-            if (fifo_size < 0) {
-                /* Если прочитать не смогли, сообщаем об ошибке
-                и завершаем работу */
-                printf("Can\'t read string at third process\n");
-                exit(-1);
-            }
-            close(fd_fifo2);
-
-            // записываем измененную строку в новый файл
-            write_size = write(fd2, buf, strlen(buf));
-
-            if (write_size != strlen(buf)) {
-                printf("2nd child Can\'t write all string\n");
-                exit(-1);
-            }
-
-            if (close(fd2) < 0) {
-                printf("Can\'t close file\n");
-            }
+        // Пробуем прочитать из fifo2 данные в массив
+        if ((fd_fifo2 = open(fifo_name2, O_RDONLY)) < 0) {
+            /* Если открыть fifo2 не удалось, печатаем об этом
+            сообщение и прекращаем работу */
+            printf("Can\'t open fifo2 for reading at third process\n");
+            exit(-1);
         }
+
+        fifo_size = read(fd_fifo2, buf, 5000);
+        if (fifo_size < 0) {
+            /* Если прочитать не смогли, сообщаем об ошибке
+            и завершаем работу */
+            printf("Can\'t read string at third process\n");
+            exit(-1);
+        }
+        close(fd_fifo2);
+
+        // записываем измененную строку в новый файл
+        write_size = write(fd2, buf, strlen(buf));
+
+        if (write_size != strlen(buf)) {
+            printf("2nd child Can\'t write all string\n");
+            exit(-1);
+        }
+
+        if (close(fd2) < 0) {
+            printf("Can\'t close file\n");
+        }
+        remove("file1.fifo");
+        remove("file2.fifo");
     }
-    remove("file1.fifo");
-    remove("file2.fifo");
 }
