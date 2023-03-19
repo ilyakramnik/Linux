9096216a0795f9e520dd247fc4734944294ad447
Ilya <kramnik.i2003@gmail.com>
2023-03-19 11:06:04 +0300

    added some fixes, block-schemes updated

--- IDZ1/5/main.c
+++ IDZ1/5/main.c
@@ -18,8 +18,8 @@ int main(int argc, char *argv[]) {
     char *input_file = (char *) calloc(5000, sizeof(char)); // название файла для чтения
     char *output_file = (char *) calloc(5000, sizeof(char)); // название файла для записи
     char temp;
-    char fifo_name[] ="file1.fifo";
-    char fifo_name2[] ="file2.fifo";
+    char fifo_name[] = "file1.fifo";
+    char fifo_name2[] = "file2.fifo";
     ssize_t read_bytes;
     size_t write_size, fifo_size;
 
@@ -29,10 +29,10 @@ int main(int argc, char *argv[]) {
     strcpy(output_file, argv[4]);
     remove(output_file);
 
-    (void)umask(0);
+    (void) umask(0);
     /* Попытаемся создать fifo с именем file1.fifo в текущей
     директории */
-    if(mknod(fifo_name, S_IFIFO | 0666, 0) < 0){
+    if (mknod(fifo_name, S_IFIFO | 0666, 0) < 0) {
         /* Если создать fifo не удалось, печатаем об этом
         сообщение и прекращаем работу */
         printf("Can\'t create fifo at process 1\n");
@@ -60,8 +60,8 @@ int main(int argc, char *argv[]) {
             printf("Can\'t close file\n");
         }
 
-        // записываем данные из buf в fifo
-        if((fd_fifo = open(fifo_name, O_WRONLY)) < 0){
+        // открываем fifo
+        if ((fd_fifo = open(fifo_name, O_WRONLY)) < 0) {
             /* Если открыть fifo не удалось, печатаем об этом
             сообщение и прекращаем работу */
             printf("Can\'t open FIFO for writing at first process\n");
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
         }
         // Пробуем записать в fifo наш buf
         fifo_size = write(fd, buf, strlen(buf));
-        if(fifo_size != strlen(buf)){
+        if (fifo_size != strlen(buf)) {
             /* Если записалось меньшее количество байт, то сообщаем
             об ошибке и завершаем работу */
             printf("Can\'t write all string to FIFO at first process\n");
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
         wait(NULL);
     } else if (chpid == 0) { // второй процесс
 
-        if((fd_fifo = open(fifo_name, O_RDONLY)) < 0){
+        if ((fd_fifo = open(fifo_name, O_RDONLY)) < 0) {
             /* Если открыть fifo не удалось, печатаем об этом
             сообщение и прекращаем работу */
             printf("Can\'t open FIFO for reading at second process\n");
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
         }
         // Пробуем прочитать из fifo данные в массив
         fifo_size = read(fd_fifo, buf, 5000);
-        if(fifo_size < 0){
+        if (fifo_size < 0) {
             /* Если прочитать не смогли, сообщаем об ошибке
             и завершаем работу */
             printf("Can\'t read string at second process\n");
@@ -106,9 +106,9 @@ int main(int argc, char *argv[]) {
         printf("%s\n", buf);
 
         // создание fifo2
-        (void)umask(0);
-        if(mknod(fifo_name2, S_IFIFO | 0666, 0) < 0){
-            /* Если создать fifo не удалось, печатаем об этом
+        (void) umask(0);
+        if (mknod(fifo_name2, S_IFIFO | 0666, 0) < 0) {
+            /* Если создать fifo2 не удалось, печатаем об этом
             сообщение и прекращаем работу */
             printf("Can\'t create fifo at process 2\n");
             exit(-1);
@@ -116,8 +116,7 @@ int main(int argc, char *argv[]) {
 
         chpid2 = fork(); // создание третьего потока
         if (chpid2 > 0) {
-            // записываем измененную строку в fifo2
-            if((fd_fifo2 = open(fifo_name2, O_WRONLY)) < 0){
+            if ((fd_fifo2 = open(fifo_name2, O_WRONLY)) < 0) {
                 /* Если открыть fifo не удалось, печатаем об этом
                 сообщение и прекращаем работу */
                 printf("Can\'t open FIFO for writing at second process\n");
@@ -125,7 +124,7 @@ int main(int argc, char *argv[]) {
             }
             // Пробуем записать в fifo2 наш buf
             fifo_size = write(fd_fifo2, buf, strlen(buf));
-            if(fifo_size != strlen(buf)){
+            if (fifo_size != strlen(buf)) {
                 /* Если записалось меньшее количество байт, то сообщаем
                 об ошибке и завершаем работу */
                 printf("Can\'t write all string to fifo2 at second process\n");
@@ -143,7 +142,7 @@ int main(int argc, char *argv[]) {
             }
 
             // Пробуем прочитать из fifo2 данные в массив
-            if((fd_fifo2 = open(fifo_name2, O_RDONLY)) < 0){
+            if ((fd_fifo2 = open(fifo_name2, O_RDONLY)) < 0) {
                 /* Если открыть fifo2 не удалось, печатаем об этом
                 сообщение и прекращаем работу */
                 printf("Can\'t open fifo2 for reading at third process\n");
@@ -151,7 +150,7 @@ int main(int argc, char *argv[]) {
             }
 
             fifo_size = read(fd_fifo2, buf, 5000);
-            if(fifo_size < 0){
+            if (fifo_size < 0) {
                 /* Если прочитать не смогли, сообщаем об ошибке
                 и завершаем работу */
                 printf("Can\'t read string at third process\n");
@@ -172,4 +171,6 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    remove("file1.fifo");
+    remove("file2.fifo");
 }
